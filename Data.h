eed42bc5c564a10ce4b247a85aa043464f3993e8
Academy_ekat@itstep.academy <Academy_ekat@itstep.academy>
2022-03-24 21:26:49 +0500

    Add project files.

---
+++ Data.h
@@ -0,0 +1,14 @@
+#pragma once
+#include <string>
+#include <iostream>
+
+
+struct DB {
+	std::string title;
+	int notesCount = 0;
+	std::string notes;
+};
+
+void addNote(DB &base);
+void clearNotes(DB& base);
+void showDB(DB& base);
